4643e0c26dc51edd07aac074e97c153087011232
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2012-01-01 07:18:48 +0000

    Exposed the force-Log4j-config feature.

--- maven/org.appdapter.bundle.core/src/main/java/org/appdapter/osgi/core/BundleActivatorBase.java
+++ maven/org.appdapter.bundle.core/src/main/java/org/appdapter/osgi/core/BundleActivatorBase.java
@@ -22,6 +22,13 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger; 
+
+import java.net.URL;
+import org.appdapter.bind.log4j.Log4jFuncs;
+
+
+
+
 public abstract class BundleActivatorBase implements BundleActivator  {
 	protected abstract Logger getLogger();
 			
@@ -40,6 +47,28 @@ public abstract class BundleActivatorBase implements BundleActivator  {
 		System.out.println("[System.out]" + windupMsg);		
 		Logger log = getLogger();
 		log.info("[SLF4J]" + windupMsg);		
+	}
+	
+	protected void forceLog4jConfig() {
+		Logger logger = getLogger();
+
+		// To get more determinism over when this happens (before other bundles that use logging are launched),
+		// need to mess with Felix auto-start properties?
+		ClassLoader threadCL = Thread.currentThread().getContextClassLoader();
+		ClassLoader localCL = getClass().getClassLoader();
+		System.out.println("thread-context-CL=" + threadCL);
+		System.out.println("local-CL=" + localCL);
+
+		String resPath = "log4j.properties";
+		
+		URL threadURL = threadCL.getResource(resPath);
+		URL localURL = localCL.getResource(resPath);
+		System.out.println("[System.out] threadCL resolved " + resPath + " to threadURL " + threadURL);
+		System.out.println("[System.out] localCL resolved  " + resPath + " to  localURL" + localURL);
+		System.out.println("[System.out] " + getClass().getCanonicalName() + " is forcing Log4J to read config from localURL: " + localURL);
+		Log4jFuncs.forceLog4jConfig(localURL);
+		getLogger().info("{forceLog4JConfig} - This here message should be conveyed by SLF4J->Log4J");
+		
 	}
 
 }
