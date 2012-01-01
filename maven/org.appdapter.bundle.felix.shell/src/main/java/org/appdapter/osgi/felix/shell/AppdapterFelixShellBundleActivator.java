a7c100ede93df499c322dccebbe9f4b4e8a805bf
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2012-01-01 07:18:48 +0000

    Exposed the force-Log4j-config feature.

--- maven/org.appdapter.bundle.felix.shell/src/main/java/org/appdapter/osgi/felix/shell/AppdapterFelixShellBundleActivator.java
+++ maven/org.appdapter.bundle.felix.shell/src/main/java/org/appdapter/osgi/felix/shell/AppdapterFelixShellBundleActivator.java
@@ -17,8 +17,7 @@
 
 package org.appdapter.osgi.felix.shell;
 
-import java.net.URL;
-import org.apache.log4j.PropertyConfigurator;
+
 import org.appdapter.gui.demo.DemoBrowser;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -27,34 +26,22 @@ import org.slf4j.LoggerFactory;
 
 import org.appdapter.gui.demo.DemoNavigatorCtrl;
 import javax.swing.JFrame;
+import org.appdapter.osgi.core.BundleActivatorBase;
 
-public class AppdapterFelixShellBundleActivator implements BundleActivator {
+public class AppdapterFelixShellBundleActivator extends BundleActivatorBase {
 	static Logger theLogger = LoggerFactory.getLogger(AppdapterFelixShellBundleActivator.class);
 	
 	private JFrame	myDemoJFrame;
 	
+	@Override protected Logger getLogger() {
+		return theLogger;
+	}	
+	
     public void start(BundleContext context) throws Exception {
-		String startupMsg = getClass().getCanonicalName() + ".start(ctx=" + context + ")";
-		System.out.println("[System.out]" + startupMsg);
-		theLogger.info("[SLF4J]" + startupMsg);
-		ClassLoader threadCL = Thread.currentThread().getContextClassLoader();
-		ClassLoader localCL = getClass().getClassLoader();
-		System.out.println("thread-context-CL=" + threadCL);
-		System.out.println("local-CL=" + localCL);
+		
+		forceLog4jConfig();
+		
 
-		String resPath = "log4j.properties";
-		
-		URL threadURL = threadCL.getResource(resPath);
-		URL localURL = localCL.getResource(resPath);
-		System.out.println("threadCL resolved " + resPath + " to " + threadURL);
-		System.out.println("localCL resolved " + resPath + " to " + localURL);
-		
-		// To get more determinism over when this happens (before other bundles that use logging are launched),
-		// need to mess with Felix auto-start properties?
-		System.out.println("[System.out] " + getClass().getCanonicalName() + " is forcing Log4J to read config from: " + localURL);
-		PropertyConfigurator.configure(localURL);
-		theLogger.info("[SLF4J]" + startupMsg);
-		theLogger.info("Is SLF4J->Log4J logging working?");
 		theLogger.info("Starting demo browser[");
 		
 		String args[] = null;
