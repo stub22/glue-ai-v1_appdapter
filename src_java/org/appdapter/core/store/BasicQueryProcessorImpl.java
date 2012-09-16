b95db2298f6aa933b1be86748b35e814f5addada
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2012-09-16 04:39:52 +0000

    Defining limited functionality for the QueryProcessor.

--- src_java/org/appdapter/core/store/BasicQueryProcessorImpl.java
+++ src_java/org/appdapter/core/store/BasicQueryProcessorImpl.java
@@ -36,75 +36,10 @@ import java.util.List;
 import org.appdapter.core.log.BasicDebugger;
 /**
  * @author Stu B. <www.texpedient.com>
+ * 
+ * Placeholder
  */
 
 public abstract class BasicQueryProcessorImpl extends BasicDebugger implements QueryProcessor {
-	// All the stateless utility features below are now static methods in JenaArqQueryFuncs
-	/*
-	public Query parseQueryText(String inlineQueryText, PrefixMapping pmap) { 
-		String qBaseURI = null;
-		Query query = new Query();
-		// Query prefixes must be applied before the query is parsed.
-		query.setPrefixMapping(pmap);
-		QueryFactory.parse(query, inlineQueryText, qBaseURI, Syntax.syntaxSPARQL); 
-		return query;
-	}
-	
-	public Query parseQueryURL(String resolvedQueryURL) {
-		Query parsedQuery = null;
-		try {
-			// String resolvedQueryURL = DemoResources.QUERY_PATH;
-			// DemoResources.resolveResourcePathToURL_WhichJenaCantUseInCaseOfJarFileRes(DemoResources.QUERY_PATH);
-			logInfo("Registering classLoader with JenaFM");  // Because it is used 
-			AssemblerUtils.ensureClassLoaderRegisteredWithJenaFM(getClass().getClassLoader());
-			parsedQuery = QueryFactory.read(resolvedQueryURL);
-		} catch (Throwable t) {
-			logError("problem in parseQueryURL", t);
-		}
-		return parsedQuery;
-	}
-	public <ResType> ResType processQueryExecution(QueryExecution qe, Repo.ResultSetProc<ResType> resProc) {
-		ResType result = null;
-		
-		try {
 
-			try {
-				// ResultSet does not have a close() method
-				ResultSet rs = qe.execSelect();
-				result = resProc.processResultSet(rs);
-			} finally {
-				qe.close();
-			}
-		} catch (Throwable t) {
-			logError("problem in QueryTrigger", t);
-		}
-		return result;
-	}
-	@Override public <ResType> ResType processDatasetQuery(Dataset ds, Query parsedQuery, QuerySolution initBinding, Repo.ResultSetProc<ResType> resProc) {
-		QueryExecution qe = QueryExecutionFactory.create(parsedQuery, ds, initBinding);
-		return processQueryExecution(qe, resProc);
-	}
-	@Override public List<QuerySolution> findAllSolutions(Dataset ds, Query parsedQuery, QuerySolution initBinding) {
-		Repo.ResultSetProc<List<QuerySolution>> resProc = new Repo.ResultSetProc<List<QuerySolution>>() {
-			@Override public List<QuerySolution> processResultSet(ResultSet rset) {
-				List<QuerySolution> solnList = new ArrayList<QuerySolution>();
-				while (rset.hasNext()) {
-					QuerySolution qsoln = rset.next();
-					solnList.add(qsoln);
-				}
-				return solnList;
-			}
-		};
-		return processDatasetQuery(ds, parsedQuery, initBinding, resProc);
-	}	
-	public static String dumpResultSetToXML(ResultSet rs) {
-		ResultSetRewindable rsr = ResultSetFactory.makeRewindable(rs);
-		// Does this print to console in table format? 
-		ResultSetFormatter.out(rsr);
-		rsr.reset();
-		String resultXML = ResultSetFormatter.asXMLString(rsr);
-		return resultXML;
-	}
-	*
-	*/ 
 }
