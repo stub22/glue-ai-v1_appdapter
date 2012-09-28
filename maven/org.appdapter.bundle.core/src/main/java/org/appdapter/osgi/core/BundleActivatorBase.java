11367d8e693a889b7c28120c4e98936f09e9d3bf
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2012-09-28 19:51:56 +0000

    Added Framework-Start callback feature to the BundleActivatorBase. Increased commentary on logging setup.

--- maven/org.appdapter.bundle.core/src/main/java/org/appdapter/osgi/core/BundleActivatorBase.java
+++ maven/org.appdapter.bundle.core/src/main/java/org/appdapter/osgi/core/BundleActivatorBase.java
@@ -27,18 +27,43 @@ import org.appdapter.core.log.BasicDebugger;
 
 import java.net.URL;
 import org.appdapter.bind.log4j.Log4jFuncs;
+import org.osgi.framework.*;
 
 
-
+/**
+ * Note:  Appdapter client bundles are *not* required to use this class, or to use
+ * Log4J logging.  However, you can choose to extend this base class, in order to 
+ * get access to some SLF4J-basd logging methods.
+ * 
+ * This BundleActivator extends BasicDebugger.  Thus, by extending it, your bundle gets
+ * access to the logging methods of that class.  This class prints messages when its
+ * start() and stop() methods are called, but otherwise takes no action. 
+ * 
+ * Optional
+ * If your bundle is the "top" bundle of some application, then you can use the
+ * forceLog4JConfig() method inherited from BasicDebugger to read config from a
+ * log4j.properties file-resource at the root of your bundle.
+ * 
+ * @author Stu B. <www.texpedient.com>
+ */
 
 public abstract class BundleActivatorBase extends BasicDebugger implements BundleActivator  {
-	// protected abstract Logger getLogger();
-		
-	protected String describe(String action, BundleContext bundleCtx) { 
-		Bundle b = bundleCtx.getBundle();
-		String msg = getClass().getCanonicalName() + "." + action + "(ctx=[" + bundleCtx + "], bundle=[" + b + "])";
-		return msg;
-	}
+
+	/**
+	 * There are two things you might do in your overriding start() method, which
+	 * are generally only done in asingle  "top" bundle (generally corresponding to a project
+	 * that you want to "run" from maven, or use as the seed of your own launch strategy).
+	 * 
+	 * If you call these methods from multiple bundles, then you will have to deal with
+	 * the fact that those invocations (and their callbacks) may generally occur in any order.
+	 * 
+	 * 1) Call forceLog4JConfig() if you like Log4J logging.  
+	 * 2) Call scheduleFrameworkStartEventHandler() to schedule a callback allowing you to
+	 * safely begin your application setup *after* all initial bundles have been started.
+	 * 
+	 * @param bundleCtx
+	 * @throws Exception 
+	 */
     @Override public void start(BundleContext bundleCtx) throws Exception {
 		logInfo(describe("start<BundleActivatorBase>", bundleCtx));
     }
@@ -46,5 +71,50 @@ public abstract class BundleActivatorBase extends BasicDebugger implements Bundl
     @Override public void stop(BundleContext bundleCtx) throws Exception {
 		logInfo(describe("stop<BundleActivatorBase>", bundleCtx));	
 	}
+
+	/** Override this method if you need notification after all bundles in your container have been started.
+	 * Normally you would override this method in a "top" bundle to setup your application code.
+	 * Important:  This handler will only be called if you called scheduleFrameworkStartEventHandler()
+	 * from your bundle's start() method!
+	 * @param bundleCtx - Fetched from the bundle of the FrameworkEvent, so it should be nice and fresh.
+	 */
+	protected void handleFrameworkStartedEvent(BundleContext bundleCtx) {
+		logInfo("Default implementation of handleFrameworkStartedEvent() called on " + getClass() + ", you should override this!  BundleContext=" + bundleCtx);
+	}
+	/**
+	 * Call this method from any bundle's start() to schedule a callback to its handleFrameworkStartedEvent() method.
+	 * 
+	 * @param bundleCtx - used to schedule the callback, and then forgotten.
+	 */
+	protected void scheduleFrameworkStartEventHandler(BundleContext bundleCtx) {
+			bundleCtx.addFrameworkListener(new FrameworkListener() {
+			public void frameworkEvent(FrameworkEvent fe) {
+				int eventType = fe.getType();
+				logInfo("************************ Got frameworkEvent with eventType=" + eventType + ", bundle=" + fe.getBundle());
+				if (eventType == FrameworkEvent.STARTED) {
+					logInfo("********  OSGi Framework has STARTED, calling dispatchFrameworkStartedEvent()");
+					dispatchFrameworkStartedEvent(fe.getBundle(), fe.getThrowable());
+				}
+			}
+		});
+	}
+	private void dispatchFrameworkStartedEvent(Bundle eventBundle, Throwable eventThrowable) {
+		String thrownMsg = (eventThrowable == null) ? "OK" : eventThrowable.getClass().getName();
+		logInfo("dispatchFrameworkStartedEvent<BundleActivatorBase> ( bundle=" + eventBundle + ", msg=" + thrownMsg);
+		if (eventThrowable == null) {
+			BundleContext bc = eventBundle.getBundleContext();
+			if (bc == null) {
+				logError("Cannot find bundle context for event bundle, so there will be no callback to app startup: " + eventBundle);
+			}
+			handleFrameworkStartedEvent(bc);
+		} else {
+			logWarning("No callback to application startup, due to throwable ", eventThrowable);
+		}
+	}
 	
+	protected String describe(String action, BundleContext bundleCtx) { 
+		Bundle b = bundleCtx.getBundle();
+		String msg = getClass().getCanonicalName() + "." + action + "(ctx=[" + bundleCtx + "], bundle=[" + b + "])";
+		return msg;
+	}	
 }
