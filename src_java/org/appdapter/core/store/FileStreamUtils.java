8b4aed5b14a2905f4121194c3adf017629faeb7d
logicmoo <logicmoo@795dda8d-9af7-4486-b49a-b618fc8168e2>
2013-04-10 17:07:11 +0000

    Moved some of the toCVS to java

--- src_java/org/appdapter/core/store/FileStreamUtils.java
+++ src_java/org/appdapter/core/store/FileStreamUtils.java
@@ -21,9 +21,15 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Reader;
+import java.io.StringReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.*;
+
+import com.hp.hpl.jena.sparql.pfunction.library.container;
 
 /**
  * @author Stu B. <www.texpedient.com>
@@ -36,6 +42,109 @@ public class FileStreamUtils {
 		if (at < 0)
 			return null;
 		return srcPath.substring(at + 1).toLowerCase();
+	}
+
+	public static Reader sheetToReader(Sheet sheet) {
+		StringBuffer sheetBuff = new StringBuffer();
+		if (sheet.getPhysicalNumberOfRows() == 0) {
+			throw new RuntimeException("No rows on sheet: " + sheet);
+		}
+		int width = getSheetWidth(sheet);
+		int maxInclusive = sheet.getLastRowNum();
+		for (int i = sheet.getFirstRowNum(); i <= maxInclusive; i++) {
+			StringBuffer strBuff = new StringBuffer();
+			Row row = sheet.getRow(i);
+
+			int rwInclusve = row.getLastCellNum();
+			for (int j = 0; j <= rwInclusve; j++) {
+				Cell cell = row.getCell(j);
+				if (j > 0)
+					strBuff.append(",");
+				if (cell == null)
+					continue;
+				switch (cell.getCellType()) {
+				case Cell.CELL_TYPE_NUMERIC:
+					strBuff.append(cell.getNumericCellValue());
+					continue;
+				case Cell.CELL_TYPE_BLANK:
+					continue;
+				case Cell.CELL_TYPE_STRING:
+					strBuff.append(escapeCSV(cell.getStringCellValue()));
+					continue;
+				case Cell.CELL_TYPE_FORMULA:
+					strBuff.append(escapeCSV(cell.getCellFormula()));
+					continue;
+				default:
+					break;
+				}
+				String str = cell.getStringCellValue();
+				if (str == null)
+					continue;
+				strBuff.append(str);
+			}
+
+			// pad the rest
+			int pad = width - rwInclusve;
+			for (int j = 0; j < pad; j++) {
+				strBuff.append(",");
+			}
+			sheetBuff.append(strBuff.toString().trim() + "\n");
+		}
+		return new StringReader(sheetBuff.toString().trim());
+	}
+
+	private static int getSheetWidth(Sheet sheet) {
+		return sheet.getRow(sheet.getFirstRowNum()).getLastCellNum() + 1;
+	}
+
+	private static Object escapeCSV(Object cellValue) {
+		if (cellValue == null)
+			return "";
+		String cellValueStr = cellValue.toString();
+		return escapeEmbeddedCharacters(cellValueStr);
+	}
+
+	private static String escapeEmbeddedCharacters(String field) {
+		StringBuffer buffer = null;
+		final String separator = ",";
+		// If the fields contents should be formatted to confrom with Excel's
+		// convention....
+		if (true) {
+
+			// Firstly, check if there are any speech marks (") in the field;
+			// each occurrence must be escaped with another set of spech marks
+			// and then the entire field should be enclosed within another
+			// set of speech marks. Thus, "Yes" he said would become
+			// """Yes"" he said"
+			if (field.contains("\"")) {
+				buffer = new StringBuffer(field.replaceAll("\"", "\\\"\\\""));
+				buffer.insert(0, "\"");
+				buffer.append("\"");
+			} else {
+				// If the field contains either embedded separator or EOL
+				// characters, then escape the whole field by surrounding it
+				// with speech marks.
+				buffer = new StringBuffer(field);
+				if ((buffer.indexOf(separator)) > -1
+						|| (buffer.indexOf("\n")) > -1) {
+					buffer.insert(0, "\"");
+					buffer.append("\"");
+				}
+			}
+			return (buffer.toString().trim());
+		}
+		// The only other formatting convention this class obeys is the UNIX one
+		// where any occurrence of the field separator or EOL character will
+		// be escaped by preceding it with a backslash.
+		else {
+			if (field.contains(separator)) {
+				field = field.replaceAll(separator, ("\\\\" + separator));
+			}
+			if (field.contains("\n")) {
+				field = field.replaceAll("\n", "\\\\\n");
+			}
+			return (field);
+		}
 	}
 
 	public static InputStream openInputStream(String srcPath,
