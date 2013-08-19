2ad7fca6102de4cd81937c4d4927d35e05878606
Douglas R. Miles <logicmoo@gmail.com>
2013-08-19 05:50:56 +0000

    Maybe stop an infinite loop (sometimes)

--- src_java/org/appdapter/bind/rdf/jena/model/JenaLiteralUtils.java
+++ src_java/org/appdapter/bind/rdf/jena/model/JenaLiteralUtils.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2012 by The Appdapter Project (www.appdapter.org).
- * 
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -175,7 +175,7 @@ public class JenaLiteralUtils {
 			RDFDatatype dt = lit.getDatatype();
 			if (dt != null) {
 				Class clz = dt.getJavaClass();
-				if (clz != null) {
+				if (clz != null && clz != type) {
 					return (T) convertRDFNodeStatic(e, clz, findComponent);
 				}
 			}
