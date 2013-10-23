ca078fa57b02030550f8f439ee1c2b47aaf3a5ba
Douglas R. Miles <logicmoo@gmail.com>
2013-10-23 19:16:09 +0000

    Initial add of the appdapter rebundling of OpenConverters / GUI

---
+++ maven/ext.bundle.openconverters/src/main/java/org/appdapter/ext/bundle/openconverters/osgi/Activator.java
@@ -0,0 +1,19 @@
+package org.appdapter.ext.bundle.openconverters.osgi;
+
+import org.osgi.framework.BundleContext;
+
+public class Activator extends org.appdapter.ext.osgi.common.ExtBundleActivatorBase {
+
+	public static void ensureConvertersClassesAreFindable() {
+		debugLoaders(org.apache.poi.common.usermodel.Hyperlink.class);
+		debugLoaders(org.openxmlformats.schemas.drawingml.x2006.chart.impl.CTArea3DChartImpl.class);
+		debugLoaders(schemasMicrosoftComOfficeExcel.impl.ClientDataDocumentImpl.class);
+		debugLoaders(au.com.bytecode.opencsv.bean.CsvToBean.class);
+	}
+
+	@Override protected void handleFrameworkStartedEvent(BundleContext bundleCtx) throws Exception {
+		debugLoaders(Activator.class);
+		super.handleFrameworkStartedEvent(bundleCtx);
+	}
+
+}
---
+++ maven/org.appdapter.demo.gui/src/test/java/org/appdapter/demo/gui/test/FirstStandaloneTest.java
@@ -0,0 +1,34 @@
+package org.appdapter.demo.gui.test;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class FirstStandaloneTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public FirstStandaloneTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(FirstStandaloneTest.class);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		org.appdapter.ext.bundle.openconverters.osgi.Activator.ensureConvertersClassesAreFindable();
+		assertTrue(true);
+	}
+}
