6f5c53e1bed31b0933fd389e16fcbf5bd335c9da
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2014-06-28 02:35:22 +0000

    Setting the stage for Jena-TDB in-memory transactional datasets, but they are not yet used.  Additional logging shows when datasets are created, and whether they are transactional.

--- maven/org.appdapter.lib.bind.jena/src/main/java/org/appdapter/core/store/dataset/AbstractDatasetFactory.java
+++ maven/org.appdapter.lib.bind.jena/src/main/java/org/appdapter/core/store/dataset/AbstractDatasetFactory.java
@@ -62,19 +62,23 @@ public abstract class AbstractDatasetFactory implements UserDatasetFactory {
 
 	@Override abstract public String getDatasetType();
 
-	/** Create an in-memory, modifiable Dataset */
+	/**
+	 * Create an in-memory, modifiable Dataset
+	 */
 	public Dataset createMem() {
 		return create(DatasetGraphFactory.createMem());
 	}
 
-	/** Create an in-memory, modifiable Dataset.
-	 * New graphs must be explicitly added using .addGraph.
+	/**
+	 * Create an in-memory, modifiable Dataset. New graphs must be explicitly added using .addGraph.
 	 */
 	public Dataset createMemFixed() {
 		return create(DatasetGraphFactory.createMemFixed());
 	}
 
-	/** Create an in-memory, modifable Dataset
+	/**
+	 * Create an in-memory, modifable Dataset
+	 *
 	 * @deprecated Use createMem
 	 */
 	final @Deprecated public Dataset create() {
@@ -83,21 +87,19 @@ public abstract class AbstractDatasetFactory implements UserDatasetFactory {
 		return create(ModelFactory.createModelForGraph(GraphFactory.createDefaultGraph()));
 	}
 
-	/** Create a named graph container based on two list of URIs.
-	 *  The first is used to create the background (unnamed graph), the
-	 *  second is used to create the collection of named graphs.
+	/**
+	 * Create a named graph container based on two list of URIs. The first is used to create the background (unnamed
+	 * graph), the second is used to create the collection of named graphs.
 	 *
-	 *  (Jena calls graphs "Models" and triples "Statements")
+	 * (Jena calls graphs "Models" and triples "Statements")
 	 *
-	 * @param uriList          graphs to be loaded into the unnamed, default graph
-	 * @param namedSourceList  graphs to be atatched as named graphs
+	 * @param uriList graphs to be loaded into the unnamed, default graph
+	 * @param namedSourceList graphs to be atatched as named graphs
 	 * @param fileManager
-	 * @param baseURI          baseURI for relative URI expansion
+	 * @param baseURI baseURI for relative URI expansion
 	 * @return Dataset
 	 */
-
 	//		abstract public Dataset create(List<String> uriList, List<String> namedSourceList, FileManager fileManager, String baseURI);
-
 	public Dataset create(List<String> uriList, List<String> namedSourceList, FileManager fileManager, String baseURI) {
 		// Fixed dataset - any GRAPH <notThere> in a query must return no match.
 		Dataset ds = createDefault();
@@ -105,102 +107,99 @@ public abstract class AbstractDatasetFactory implements UserDatasetFactory {
 		return ds;
 	}
 
-	/** Create a dataset based on a list of URIs : these are merged into the default graph of the dataset.
+	/**
+	 * Create a dataset based on a list of URIs : these are merged into the default graph of the dataset.
 	 *
-	 * @param uriList   URIs merged to form the default dataset
+	 * @param uriList URIs merged to form the default dataset
 	 * @return Dataset
 	 */
-
 	public Dataset create(List<String> uriList) {
 		return create(uriList, null, null, null);
 	}
 
-	/** Create a dataset with a default graph and no named graphs
+	/**
+	 * Create a dataset with a default graph and no named graphs
 	 *
-	 * @param uri   URIs merged to form the default dataset
+	 * @param uri URIs merged to form the default dataset
 	 * @return Dataset
 	 */
-
 	public Dataset create(String uri) {
 		return create(uri, null, null, null);
 	}
 
-	/** Create a dataset based on a list of URIs : these are merged into the default graph of the dataset.
+	/**
+	 * Create a dataset based on a list of URIs : these are merged into the default graph of the dataset.
 	 *
-	 * @param uriList   URIs merged to form the default dataset
+	 * @param uriList URIs merged to form the default dataset
 	 * @param fileManager
 	 * @return Dataset
 	 */
-
 	public Dataset create(List<String> uriList, FileManager fileManager) {
 		return create(uriList, null, fileManager, null);
 	}
 
-	/** Create a dataset based on a list of URIs : these are merged into the default graph of the dataset.
+	/**
+	 * Create a dataset based on a list of URIs : these are merged into the default graph of the dataset.
 	 *
-	 * @param uri              graph to be loaded into the unnamed, default graph
+	 * @param uri graph to be loaded into the unnamed, default graph
 	 * @param fileManager
 	 * @return Dataset
 	 */
-
 	public Dataset create(String uri, FileManager fileManager) {
 		return create(uri, null, fileManager, null);
 	}
 
-	/** Create a named graph container of graphs based on a list of URIs.
+	/**
+	 * Create a named graph container of graphs based on a list of URIs.
 	 *
 	 * @param namedSourceList
 	 * @param fileManager
 	 * @return Dataset
 	 */
-
 	public Dataset createNamed(List<String> namedSourceList, FileManager fileManager) {
 		return create((List<String>) null, namedSourceList, fileManager, null);
 	}
 
-	/** Create a dataset based on two list of URIs.
-	 *  The first lists is used to create the background (unnamed graph) by merging, the
-	 *  second is used to create the collection of named graphs.
+	/**
+	 * Create a dataset based on two list of URIs. The first lists is used to create the background (unnamed graph) by
+	 * merging, the second is used to create the collection of named graphs.
 	 *
-	 *  (Jena calls graphs "Models" and triples "Statements")
+	 * (Jena calls graphs "Models" and triples "Statements")
 	 *
-	 * @param uriList          graphs to be loaded into the unnamed, default graph
-	 * @param namedSourceList  graphs to be atatched as named graphs
+	 * @param uriList graphs to be loaded into the unnamed, default graph
+	 * @param namedSourceList graphs to be atatched as named graphs
 	 * @return Dataset
 	 */
-
 	public Dataset create(List<String> uriList, List<String> namedSourceList) {
 		return create(uriList, namedSourceList, null, null);
 	}
 
-	/** Create a dataset container based on two list of URIs.
-	 *  The first is used to create the background (unnamed graph), the
-	 *  second is used to create the collection of named graphs.
+	/**
+	 * Create a dataset container based on two list of URIs. The first is used to create the background (unnamed graph),
+	 * the second is used to create the collection of named graphs.
 	 *
-	 *  (Jena calls graphs "Models" and triples "Statements")
+	 * (Jena calls graphs "Models" and triples "Statements")
 	 *
-	 * @param uri              graph to be loaded into the unnamed, default graph
-	 * @param namedSourceList  graphs to be attached as named graphs
+	 * @param uri graph to be loaded into the unnamed, default graph
+	 * @param namedSourceList graphs to be attached as named graphs
 	 * @return Dataset
 	 */
-
 	public Dataset create(String uri, List<String> namedSourceList) {
 		return create(uri, namedSourceList, null, null);
 	}
 
-	/** Create a named graph container based on two list of URIs.
-	 *  The first is used to create the background (unnamed graph), the
-	 *  second is used to create the collection of named graphs.
+	/**
+	 * Create a named graph container based on two list of URIs. The first is used to create the background (unnamed
+	 * graph), the second is used to create the collection of named graphs.
 	 *
-	 *  (Jena calls graphs "Models" and triples "Statements")
+	 * (Jena calls graphs "Models" and triples "Statements")
 	 *
-	 * @param uri              graph to be loaded into the unnamed, default graph
-	 * @param namedSourceList  graphs to be atatched as named graphs
+	 * @param uri graph to be loaded into the unnamed, default graph
+	 * @param namedSourceList graphs to be atatched as named graphs
 	 * @param fileManager
-	 * @param baseURI          baseURI for relative URI expansion
+	 * @param baseURI baseURI for relative URI expansion
 	 * @return Dataset
 	 */
-
 	public Dataset create(String uri, List<String> namedSourceList, FileManager fileManager, String baseURI) {
 		List<String> uriList = new ArrayList<String>();
 		uriList.add(uri);
@@ -212,14 +211,12 @@ public abstract class AbstractDatasetFactory implements UserDatasetFactory {
 	//	        DataSourceImpl ds2 = new DataSourceImpl(ds) ;
 	//	        return ds2 ;
 	//	    }
-
 	//	     public  Dataset make(Dataset ds, Graph defaultGraph)
 	//	    {
 	//	        DataSourceImpl ds2 = new DataSourceImpl(ds) ;
 	//	        ds2.setDefaultGraph(defaultGraph) ;
 	//	        return ds2 ;
 	//	    }
-
 	public Dataset make(Dataset ds, Model defaultModel) {
 		Dataset ds2 = new DatasetImpl(ds);
 		ds2.setDefaultModel(defaultModel);
@@ -227,9 +224,10 @@ public abstract class AbstractDatasetFactory implements UserDatasetFactory {
 	}
 
 	// Assembler.
-	/** Assembler a dataset from the model in a file
+	/**
+	 * Assembler a dataset from the model in a file
 	 *
-	 * @param filename      The filename
+	 * @param filename The filename
 	 * @return Dataset
 	 */
 	public Dataset assemble(String filename) {
@@ -237,10 +235,11 @@ public abstract class AbstractDatasetFactory implements UserDatasetFactory {
 		return assemble(model);
 	}
 
-	/** Assembler a dataset from the model in a file
+	/**
+	 * Assembler a dataset from the model in a file
 	 *
-	 * @param filename      The filename
-	 * @param  resourceURI  URI for the dataset to assembler
+	 * @param filename The filename
+	 * @param resourceURI URI for the dataset to assembler
 	 * @return Dataset
 	 */
 	public Dataset assemble(String filename, String resourceURI) {
@@ -249,39 +248,45 @@ public abstract class AbstractDatasetFactory implements UserDatasetFactory {
 		return assemble(r);
 	}
 
-	/** Assembler a dataset from the model
+	/**
+	 * Assembler a dataset from the model
 	 *
 	 * @param model
 	 * @return Dataset
 	 */
 	public Dataset assemble(Model model) {
 		Resource r = GraphUtils.findRootByType(model, DatasetAssembler.getType());
-		if (r == null)
+		if (r == null) {
 			throw new ARQException("No root found for type <" + DatasetAssembler.getType() + ">");
+		}
 
 		return assemble(r);
 	}
 
-	/** Assembler a dataset from a resource
+	/**
+	 * Assembler a dataset from a resource
 	 *
-	 * @param resource  The resource for the dataset
+	 * @param resource The resource for the dataset
 	 * @return Dataset
 	 */
-
 	public Dataset assemble(Resource resource) {
 		Dataset ds = (Dataset) Assembler.general.open(resource);
 		return ds;
 	}
 
 	@Override public boolean canCreateType(String typeOf, String sharedNameIgnoredPresently) {
-		Map<String, UserDatasetFactory> registeredUserDatasetFactoryByName = RepoDatasetFactory.registeredUserDatasetFactoryByName;
-		synchronized (registeredUserDatasetFactoryByName) {
-			if (typeOf != null && registeredUserDatasetFactoryByName.get(typeOf) == this)
+		if (typeOf != null) {
+			UserDatasetFactory tyof = RepoDatasetFactory.getUserDF_forName(typeOf);
+			if (tyof == this) {
 				return true;
-			if (sharedNameIgnoredPresently != null && registeredUserDatasetFactoryByName.get(sharedNameIgnoredPresently) == this)
+			}
+		}
+		if (sharedNameIgnoredPresently != null) {
+			UserDatasetFactory snip = RepoDatasetFactory.getUserDF_forName(sharedNameIgnoredPresently);
+			if (snip == this) {
 				return true;
+			}
 		}
 		return false;
 	}
-
 }
