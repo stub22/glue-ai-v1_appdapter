debada6112f4bee47dc567a849d4fa69bafde6ab
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2014-07-13 14:21:27 +0000

    Testing Junit4 config.

--- test_java/org/appdapter/test/sanity/BreakfastTest.java
+++ test_java/org/appdapter/test/sanity/BreakfastTest.java
@@ -15,12 +15,17 @@
  */
 package org.appdapter.test.sanity;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
+import junit.runner.Version;
 /**
  *
  * @author Stu B. <www.texpedient.com>
@@ -28,10 +33,12 @@ import org.junit.Test;
 public class BreakfastTest {
 	
 	public BreakfastTest() {
+		System.out.println("Behold the morning - a BreakfastTest is constructed!");
 	}
 
 	@BeforeClass
 	public static void setUpClass() throws Exception {
+			System.out.println("Yes, let's setUpClass");
 	}
 
 	@AfterClass
@@ -40,19 +47,63 @@ public class BreakfastTest {
 	
 	@Before
 	public void setUp() {
+		System.out.println("setUp is happenin");
 	}
 	
 	@After
 	public void tearDown() {
+		System.out.println("dun got tore down");
 	}
-	// TODO add test methods here.
-	// The methods must be annotated with annotation @Test. For example:
-	//
-	@Test
-	public void boilSomeWater() {}
-	@Test
-	public void scrambleTheEggs() {}	
-	@Test
-	public void toastTheBagel() {}	
 
+	@Test
+	public void boilSomeWater() {
+		System.out.println("Yea, I smell the water boiling");
+	}
+	@Test
+	public void scrambleTheEggs() {
+		System.out.println("scram-scram delish");
+		        Collection collection = new ArrayList();
+        assertTrue(collection.isEmpty());
+	}
+	@Ignore("No time for toast test today, ma!")
+	@Test
+	public void toastTheBagel() {
+		System.out.println("The bagel MUST be toasty!");
+	}
+	
+	/*
+	 * If you are running your JUnit 4 tests with a JUnit 3.x runner, write a suite() method that uses the JUnit4TestAdapter class to create a suite containing all of your test methods:
+
+    public static junit.framework.Test suite() {
+        return new junit.framework.JUnit4TestAdapter(SimpleTest.class);
+    }
+	 */
+    public static junit.framework.Test suite() {
+        return new junit.framework.JUnit4TestAdapter(BreakfastTest.class);
+    }	
+	/*
+	 * Still getting:
+	 * [surefire:test]
+Surefire report directory: E:\_data\_mount\appdapter_trunk\maven\org.appdapter.lib.core\target\surefire-reports
+Behold the morning - a BreakfastTest is constructed!
+
+-------------------------------------------------------
+ T E S T S
+-------------------------------------------------------
+Running org.appdapter.test.sanity.BreakfastTest
+Tests run: 0, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.001 sec
+
+Results :
+
+Tests run: 0, Failures: 0, Errors: 0, Skipped: 0
+	 * 
+	 * 
+	 * http://stackoverflow.com/questions/2332832/no-tests-found-with-test-runner-junit-4 
+	 * I solved it by renaming one of the test methods to start with "test..." (JUnit3 style) 
+	 * and then all tests are found. I renamed it back to what it was previously, and it still works.
+	 */
+	public void testJunit473IsJunk() { 
+		System.out.println("Testing that junit4.7.3 is a pile of poo");
+		System.out.println("Actual JUnit version = " + Version.id());
+	}
 }
