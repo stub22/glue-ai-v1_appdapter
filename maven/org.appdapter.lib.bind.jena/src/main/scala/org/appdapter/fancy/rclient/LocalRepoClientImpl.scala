c6699997640b4d0122e298139701796614c73bf2
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2014-09-03 15:39:29 +0000

    LocalRepoClientImpl.getNamedModelReadonly now uses a read-transaction to fetch the model and make a copy.

--- maven/org.appdapter.lib.bind.jena/src/main/scala/org/appdapter/fancy/rclient/LocalRepoClientImpl.scala
+++ maven/org.appdapter.lib.bind.jena/src/main/scala/org/appdapter/fancy/rclient/LocalRepoClientImpl.scala
@@ -21,12 +21,14 @@ import org.appdapter.core.name.Ident
 
 import org.appdapter.core.model.{RdfNodeTranslator}
 import org.appdapter.core.query.{InitialBinding}
-import org.appdapter.core.store.{ Repo}
+import org.appdapter.core.store.{ Repo, RepoQueryFuncs_TxAware}
+import org.appdapter.bind.rdf.jena.query.JenaArqQueryFuncs_TxAware.Oper
 
 import org.appdapter.fancy.model.{ModelClientCore, ModelClientImpl}
 
 import org.appdapter.fancy.query.{SolutionList, SolutionHelper}
 
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 /**
  * @author Stu B. <www.texpedient.com>
  */
@@ -54,9 +56,29 @@ class LocalRepoClientImpl(private val myRepo: Repo.WithDirectory,  dTgtGraphVarN
 		myFixmeCachedModelClient
 	}
 	override def getNamedModelReadonly(graphID: org.appdapter.core.name.Ident): com.hp.hpl.jena.rdf.model.Model = {
+		
 		val r = getRepoIfLocal
 		if (r != null) {
-			r.getNamedModel(graphID)
+			// See comment about copies in the RepoClient trait.
+			
+			// Repo returns a handle to a local model (typically within a jena-arq-dataset).
+			// We (usually) must be in a transaction when we make this call.
+			// IF we are already in a transaction, then it would be permitted to return the handle, rather than a copy.
+			// However, that would be purely as a performance optimization, and should not have any logical impact,
+			// because the same data is contained in a read-locked model as in a copy of that model.
+			// 
+			// We are currently assuming such an optimization is not necessary, so we always make a copy.
+			//  val futureOptFlag = in transaction already?
+			val copiedModel : Model = RepoQueryFuncs_TxAware.execReadTransCompatible(r, null, new Oper[Model]() { 
+				override def perform(): Model = {
+					val modelHandleInTrans = r.getNamedModel(graphID)
+					val copy = ModelFactory.createDefaultModel();
+					// See notes about future possible optimization above.       if (futureOptFlag) then don't copy
+					copy.add(modelHandleInTrans)
+					copy
+				}
+			})
+			copiedModel
 		} else null
 	}
   
