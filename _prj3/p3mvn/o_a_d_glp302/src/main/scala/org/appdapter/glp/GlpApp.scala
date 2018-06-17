fab4a15169d98bdfbd1ba72778dcaa0fc69b7f47
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2018-06-17 11:39:15 +0000

    Saving some experimental side projects.

---
+++ _prj3/p3mvn/o_a_d_glp302/src/main/scala/org/appdapter/glp/GlpApp.scala
@@ -0,0 +1,59 @@
+package org.appdapter.glp
+
+import org.apache.jena.atlas.logging.LogCtl
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives.{path, _}
+import akka.stream.ActorMaterializer
+
+import scala.io.StdIn
+/**
+ * @author ${user.name}
+ */
+object GlpApp {
+  val pathA = "patha"
+  val pathB = "pathb"
+  val pathF = "pathf"
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+
+    LogCtl.setLog4j
+
+    import org.slf4j.LoggerFactory
+    val logger = LoggerFactory.getLogger(classOf[App])
+    logger.warn("logger warning whee")
+    launchWebServer
+  }
+  def launchWebServer : Unit = {
+    implicit val system = ActorSystem("my-system")
+    implicit val materializer = ActorMaterializer()
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
+
+    val route =
+      path(pathA) {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+        }
+      } ~ // note tilde connects to next case
+      path(pathB) {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say goodbye to akka-http</h1>"))
+        }
+      } ~ path(pathF) {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Foo!</h1>"))
+      }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+            .flatMap(_.unbind()) // trigger unbinding from the port
+            .onComplete(_ => system.terminate()) // and shutdown when done
+  }
+}
---
+++ _prj3/p3mvn/o_a_d_glp302/src/main/scala/org/appdapter/glp/SomeDataStuff.scala
@@ -0,0 +1,5 @@
+package org.appdapter.glp
+
+class SomeDataStuff {
+
+}
